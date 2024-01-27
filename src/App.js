2d0e0bb670338b235da95e6c0bb77207e5ebe545
Azamat Gaparov <agaparovkg@gmail.com>
2024-01-26 19:55:57 -0800

    New changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+// import Button from "@mui/material/Button";
+
+import { Box, Button, Stack } from "@mui/material";
+import Leftbar from "./components/Leftbar";
+import { Feed } from "@mui/icons-material";
+import Rightbar from "./components/Rightbar";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div className="App">Hello</div>;
+  const styles = { backgroundColor: "skyblue", color: "white" };
+
+  return (
+    // <Box bgcolor="skyblue">
+    // <Box sx={styles}>
+    <Box>
+      <Navbar />
+      <Stack direction="row" justifyContent="space-between">
+        <Leftbar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
-import { StateProvider } from "./context-api/context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StateProvider>
-      <App />
-    </StateProvider>
+    <App />
   </React.StrictMode>
 );
