e5157e76b405cdc474eb475b98f05ac1458db751
Azamat Gaparov <agaparovkg@gmail.com>
2024-01-26 18:01:09 -0800

    New changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { BASE_URL } from "./urls";
+import { FaFolder, FaFile } from "react-icons/fa";
 
 function App() {
+  const [data, setData] = useState(null);
+  // const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch(BASE_URL);
+      const data = await res.json();
+      setData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HelperFunction data={data} />
     </div>
   );
 }
 
 export default App;
+
+function HelperFunction({ data }) {
+  return (
+    <ul>
+      {data &&
+        data.map((item, index) => {
+          if (item.type === "file") {
+            return (
+              <li key={index}>
+                <FaFile className="me-2" />
+                {item.name}
+              </li>
+            );
+          }
+
+          return (
+            <li key={index}>
+              <FaFolder style={{ color: "red" }} className="me-2" />
+              {item.name}
+              <HelperFunction data={item.children} />
+            </li>
+          );
+        })}
+      {/* <li>
+        <FaFolder style={{ color: "red" }} className="me-2" />
+        public
+        <ul>
+          <li>
+            <FaFile className="me-2" />
+            child 1
+          </li>
+        </ul>
+      </li>
+      <li>
+        <FaFolder style={{ color: "red" }} className="me-2" />
+        src
+        <ul>
+          <li>
+            <FaFile className="me-2" />
+            child 1
+          </li>
+          <li>child 2</li>
+        </ul>
+      </li> */}
+    </ul>
+  );
+}
---
+++ src/urls.js
@@ -0,0 +1,2 @@
+export const BASE_URL =
+  "https://65b42574770d43aba47af813.mockapi.io/api/v1/data";
