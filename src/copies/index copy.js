17305c6c1cb4c721fc44b45b8b106d5b7d6695cd
Azamat Gaparov <agaparovkg@gmail.com>
2024-01-26 18:39:34 -0800

    New changes

--- src/copies/index copy.js
+++ src/copies/index copy.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./App";
+import App from "../App";
+import data from "./data";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App data={data} />
   </React.StrictMode>
 );
