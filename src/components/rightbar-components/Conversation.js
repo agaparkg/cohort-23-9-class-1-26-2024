68ad0fbf748aa1bdeb83a1ff3b7f6313f6516526
Azamat Gaparov <agaparovkg@gmail.com>
2024-01-27 22:20:40 -0800

    New changes

---
+++ src/components/rightbar-components/Conversation.js
@@ -0,0 +1,40 @@
+import {
+  Avatar,
+  Divider,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import React from "react";
+
+const Conversation = ({ name, primary, secondary, img, subText }) => {
+  return (
+    <>
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt={name} src={img} />
+        </ListItemAvatar>
+        <ListItemText
+          primary={primary}
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: "inline" }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                {secondary}
+              </Typography>
+              {subText}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+    </>
+  );
+};
+
+export default Conversation;
---
+++ src/data/convos.js
@@ -0,0 +1,25 @@
+const convos = [
+  {
+    name: "Remy Sharp",
+    primary: "Brunch this weekend?",
+    secondary: "Ali Connors",
+    img: "",
+    subText: " — I'll be in your neighborhood doing errands this…",
+  },
+  {
+    name: "Alex Jones",
+    primary: "Summer BBQ",
+    secondary: "to Scott, Alex, Jennifer",
+    img: "",
+    subText: " — Wish I could come, but I'm out of town this…",
+  },
+  {
+    name: "Cindy Baker",
+    primary: "Oui Oui",
+    secondary: "Sandra Adams",
+    img: "",
+    subText: " — Do you have Paris recommendations? Have you ever…",
+  },
+];
+
+export default convos;
